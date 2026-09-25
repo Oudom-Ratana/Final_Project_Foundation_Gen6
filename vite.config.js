a06aa0604ffe53167f0149dd766fb619a343fe00
Oudom-Ratana <ratanaoudom997@gmail.com>
2026-09-25 17:58:39 +0700

    prepare hosting dynamic website for testing

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+  server: {
+    proxy: {
+      "/cinema-api": {
+        target: "https://cinema-booking-api.eunglyzhia.com/api/v1",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/cinema-api/, ""),
+      },
+      "/uploads": {
+        target: "https://cinema-booking-api.eunglyzhia.com",
+        changeOrigin: true,
+      },
+    },
+  },
+});
